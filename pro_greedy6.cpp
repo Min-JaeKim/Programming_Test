45fc2e7807056e5f09a31816f72907e6b65551ac
ivo___ovi <ivo___ovi@naver.com>
2020-09-21 22:57:14 +0900

    camera

---
+++ pro_greedy6.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<vector<int>> routes) {
+    int answer = 1;
+    int temp = routes[0][1];
+    sort(routes.begin(),routes.end());
+    for(auto a : routes){
+        if(temp < a[0]){
+            answer++;
+            temp = a[1];
+        }
+        if(temp >= a[1]) temp = a[1];
+    }
+    return answer;
+}
