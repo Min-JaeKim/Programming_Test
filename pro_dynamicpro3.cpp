f3c33f0c28bb3726b61c07ad00ad3363f587e11a
ivo___ovi <ivo___ovi@naver.com>
2020-10-03 17:37:05 +0900

    back_road

---
+++ pro_dynamicpro3.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int m, int n, vector<vector<int>> puddles) {
+    int answer = 0;
+    int map[101][101] = {0,};
+    int ans[101][101] = {0,};
+    for(int i = 0; i<puddles.size(); i++)
+        map[puddles[i][1]][puddles[i][0]] = 1;
+    ans[1][0] = 1;
+    for(int i = 1; i<=n; i++){
+        for(int j = 1; j<=m; j++){
+            if(map[i][j] == 1) ans[i][j] = 0;
+            else ans[i][j] = (ans[i-1][j] + ans[i][j-1]) % 1000000007;
+        }
+    }
+    answer = ans[m][n];
+    return answer;
+}
