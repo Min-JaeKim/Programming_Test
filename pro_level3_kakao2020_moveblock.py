94e4fd6e3d8df24c99d8e98f2a049736f82c9fef
Min-JaeKim <minzminzm@gmail.com>
2021-07-14 23:32:11 +0900

    O_071421_graph

---
+++ pro_level3_kakao2020_moveblock.py
@@ -0,0 +1,50 @@
+# Python 
+
+## pro level3 블록 이동하기
+
+https://programmers.co.kr/learn/courses/30/lessons/60063
+
+> 
+
+
+
+* 문제
+
+  > 
+
+* 입력
+
+  > 
+  >
+  > ```bash
+  > 
+  > ```
+  
+* 출력
+
+  > 
+  >
+  > ```bash
+  > 
+  > ```
+  
+  
+
+```python
+
+```
+
+>
+
+
+
+* 모범답안
+
+  
+
+  ```python
+  
+  ```
+  
+  > 
+
